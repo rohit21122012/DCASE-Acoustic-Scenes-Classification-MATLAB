7e0e49fd2300a35b9271d107e1934766f52cd1e5
Rohit Patiyal <rohit21122012@gmail.com>
2015-12-31 20:31:31 +0530

    Corrected errors

--- getFeatures.m
+++ getFeatures.m
@@ -1,20 +1,26 @@
-function [allFileId, allFeature, allClass] = getFeatures( filesPath )
-%Matrices are fileId, feature, class
+function [allFileId, allFeatures, allClass] = getFeatures( filesPath )
+%Matrices are fileId, features, class
+  
   tic;
-  files = dir([filesPath, '*wav']);
+  display(['Extracting Features from Audio Files..']);
+  allFileId = [];
+  allFeatures = [];
+  allClass = [];   
+  
+  files = dir([filesPath, '*.wav']);
   for i = 1:length(files)
     fileName = files(i).name;
-    className = regexp(fileName, 'a-z+', 'match'); %'bus' 'wav'
-    className = className{1};
-
+    fileNameParts = regexp(fileName, '[a-z]+', 'match'); %'bus' 'wav'
+    className = fileNameParts{1};
+    display(['Reading ... FileNo : ', '[', num2str(i), '/', num2str(length(files)), ...
+'],  FileName : ', fileName]);
     [audio, sr] = audioread([filesPath fileName]);
     features = stFeatureExtraction(audio, sr, 0.030, 0.015)';
     numFeatures = length(features);
 
-    allFeature = [allFeature; features];
+    allFeatures = [allFeatures; features];
     allFileId = [allFileId; repmat(i, numFeatures, 1)];
     allClass = [allClass ;  nominal(repmat(className, numFeatures, 1))];
   end
   toc;
-  save('features.mat');
 end
--- run.m
+++ run.m
@@ -2,3 +2,4 @@ config
 
 [allFileId, allFeature, allClass] = ...
 getFeatures('../Data/Scene/private_sample/');
+save('features.mat');
